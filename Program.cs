6a3d81936d3a8952143b762bc4456f467822cabd
Michael Bender <mbender@primus-tech.com>
2019-08-21 08:51:21 -0400

    create program

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GradeMike
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
